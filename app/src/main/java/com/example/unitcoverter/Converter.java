a66ac2eb30796c23129465806a4f03f4d7a7aad8
JustinW1395 <justinwilcox95@hotmail.com>
2021-02-14 18:47:40 -0800

    added 2 more conversions

--- app/src/main/java/com/example/unitcoverter/Converter.java
+++ app/src/main/java/com/example/unitcoverter/Converter.java
@@ -1,7 +1,13 @@
 package com.example.unitcoverter;
 
 public class Converter {
-    public static float toCelcius(float fah) { return (fah - 32) * 5/9;
-    }
+    public static double toCelcius(double fah) { return (fah - 32) * 5/9;}
+
     public static double toKG (double lbs) { return lbs * 0.45359237;}
+
+    public static double toKil (double miles) { return miles * 1.609;}
+
+    public static double toMet (double ft) { return ft / 3.281;}
+
+
 }
