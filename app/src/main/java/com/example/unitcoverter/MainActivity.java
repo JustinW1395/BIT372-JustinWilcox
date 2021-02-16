ca96a38924cfb79bc5803f2763860be11cdabcf7
JustinW1395 <justinwilcox95@hotmail.com>
2021-02-15 17:50:10 -0800

    Added a spinner to the layout

--- app/src/main/java/com/example/unitcoverter/MainActivity.java
+++ app/src/main/java/com/example/unitcoverter/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.unitcoverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button fbtn = findViewById(R.id.convert_btn);
+        Spinner spinner = findViewById(R.id.conversion_select);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.conversions,
+                android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         EditText lbsinput = findViewById(R.id.pound_input);
         TextView kgtxt = findViewById(R.id.kilogram_text);
